f6162dd7445a6e306df3d5c44ed14cb160413daf
MashudiSudonym <muhamadmashudiardiwinata@gmail.com>
2017-03-06 19:30:39 +0700

    Remove menu.xml, add list tes

--- app/src/main/java/com/example/sudonym/todolisttgs2/MainActivity.java
+++ app/src/main/java/com/example/sudonym/todolisttgs2/MainActivity.java
@@ -3,19 +3,30 @@ package com.example.sudonym.todolisttgs2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private ArrayList<String> items;
+    private ArrayAdapter<String> itemsAdapter;
+    private ListView lvItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // taruh sini
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        items = new ArrayList<String>();
+        itemsAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                items);
+        lvItems.setAdapter(itemsAdapter);
+        items.add("Item Pertama");
+        items.add("Item Kedua");
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu, this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
 }
